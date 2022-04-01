1d48bcf73fa283b06562418bb8a95f332d0b6e21
Benjamin Fang <benjaminfang.ol@outlook.com>
2022-04-01 10:43:15 +0800

    This is besdtool, tools to operate besd files and its library.

---
+++ lib/besdtype.h
@@ -0,0 +1,15 @@
+#ifndef __BESD_TYPE__
+#define __BESD_TYPE__
+
+#define DENSE 5
+#define SMR_DENSE_3 5
+
+#define SPARSE 3
+#define SMR_SPARSE_3 3
+#define SMR_SPARSE_3F 0x40400000
+
+//next version of besd dense and sparse format
+#define DENSE2 13
+#define SPARSE2 14
+
+#endif
