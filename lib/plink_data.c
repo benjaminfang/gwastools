57248f0297ddf6d4b73535a1838e78aaa1d57bc6
Benjamin Fang <benjaminfang.ol@outlook.com>
2023-02-16 10:49:25 +0800

    updata the old lib

--- lib/plink_data.c
+++ lib/plink_data.c
@@ -1,3 +1,5 @@
+#define PLINK_DATA_SRC
+
 #include <ctype.h>
 #include <math.h>
 #include <stdbool.h>
@@ -6,55 +8,30 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "plink_file.h"
+#include "plink_data.h"
 
-static int
-clean_bim_data(BIM_DATA_ptr data_in)
-{
-    if (!data_in) {
-        return 0;
-    }
-    if (data_in->bim_data_ptr_array) {
-        free(data_in->bim_data_ptr_array);
-        data_in->bim_data_ptr_array = NULL;
-    }
-    if (data_in->current_node) {
-        data_in->current_node = NULL;
-    }
-    BIM_NODE_ptr last_node = NULL, next_node = NULL;
-    last_node = data_in->bim_data;
-    data_in->bim_data = NULL;
-
-    while(last_node) {
-        next_node = last_node->next;
-        free(last_node);
-        last_node = next_node;
-        }
-    
-    free(data_in);
-    return 0;
-}
-
-
-static BIM_DATA_ptr
+BIM_DATA_ptr
 read_bim_file(const char *bim_filename)
 {
-    BIM_DATA_ptr data_out = (BIM_DATA_ptr) malloc(sizeof(BIM_DATA));
+#if defined DEBUG_INFO_abc || DEBUG_INFO
+    printf("bim file: %s\n", bim_filename);
+#endif
+    FILE * fin = fopen(bim_filename, "r");
+    if (!fin) {
+        fprintf(stderr, "open bim file failed.\n");
+        return NULL;
+    }
+
+    BIM_DATA_ptr data_out = (BIM_DATA_ptr)malloc(sizeof(BIM_DATA));
     if (!data_out) {
         fprintf(stderr, "malloc bim data failed.\n");
         return NULL;
     }
     data_out->line_num = 0;
     data_out->current_node_index = 0;
-    data_out->current_node = NULL;
+    data_out->next_iter = NULL;
     data_out->bim_data = NULL;
-    data_out->bim_data_ptr_array = NULL;
-    
-    FILE * fin = fopen(bim_filename, "r");
-    if (!fin) {
-        fprintf(stderr, "open bim file failed.\n");
-        return NULL;
-    }
+    data_out->node_ptr_array = NULL;
 
     char line_buf[2048];
     line_buf[2047] = '\0';
@@ -182,7 +159,7 @@ read_bim_file(const char *bim_filename)
         if (!first_node) {
             first_node = new_node;
             node_ptr = new_node;
-            data_out->current_node = new_node;
+            data_out->next_iter = new_node;
             data_out->bim_data = new_node;
             data_out->line_num++;
         } else {
@@ -191,55 +168,62 @@ read_bim_file(const char *bim_filename)
             data_out->line_num++;
         }
     }
+    fclose(fin);
     return data_out;
 }
 
 
-static int
-clean_fam_data(FAM_DATA_ptr data_in)
+int
+clean_bim_data(BIM_DATA_ptr data_in)
 {
     if (!data_in) {
         return 0;
     }
-    if (data_in->fam_data_ptr_array) {
-        free(data_in->fam_data_ptr_array);
-        data_in->fam_data_ptr_array = NULL;
+    if (data_in->node_ptr_array) {
+        free(data_in->node_ptr_array);
+        data_in->node_ptr_array = NULL;
     }
-    if (data_in->current_node) {
-        data_in->current_node = NULL;
+    if (data_in->next_iter) {
+        data_in->next_iter = NULL;
     }
-    FAM_NODE_ptr last_node = NULL, next_node = NULL;
-    last_node = data_in->fam_data;
-    data_in->fam_data = NULL;
-    while (last_node) {
+    BIM_NODE_ptr last_node = NULL, next_node = NULL;
+    last_node = data_in->bim_data;
+    data_in->bim_data = NULL;
+
+    while(last_node) {
         next_node = last_node->next;
         free(last_node);
         last_node = next_node;
     }
+    
     free(data_in);
     return 0;
 }
 
 
-static FAM_DATA_ptr
+FAM_DATA_ptr
 read_fam_file(const char *fam_filename)
 {
-    FAM_DATA_ptr data_out = (FAM_DATA_ptr)malloc(sizeof(FAM_DATA));
-    if (!data_out) {
-        fprintf(stderr, "malloc for fam data failed.\n");
-        return NULL;
-    }
-    data_out->line_num = 0;
-    data_out->current_node = NULL;
-    data_out->current_node_index = 0;
-    data_out->fam_data = NULL;
-    data_out->fam_data_ptr_array = NULL;
+#if defined DEBUG_INFO_abc || DEBUG_INFO
+    printf("fam file: %s\n", fam_filename);
+#endif
 
     FILE *fin = fopen(fam_filename, "r");
     if (!fin) {
         fprintf(stderr, "open fam file failed.\n");
         return NULL;
     }
+
+    FAM_DATA_ptr data_out = (FAM_DATA_ptr)malloc(sizeof(FAM_DATA));
+    if (!data_out) {
+        fprintf(stderr, "malloc for fam data failed.\n");
+        return NULL;
+    }
+    data_out->line_num = 0;
+    data_out->next_iter = NULL;
+    data_out->current_node_index = 0;
+    data_out->fam_data = NULL;
+    data_out->node_ptr_array = NULL;
 
     char line_buf[512];
     line_buf[511] = '\0';
@@ -264,7 +248,8 @@ read_fam_file(const char *fam_filename)
             return NULL;
         }
 
-        if (sscanf(line_buf, "%s %s %s %s %s %s") != 6) {
+        if (sscanf(line_buf, "%s %s %s %s %s %s", fam_id, within_famid, 
+            father_id, mother_id, sex, pheno_val) != 6) {
             fprintf(stderr, "split fam line field failed.\n");
             clean_fam_data(data_out);
             return NULL;   
@@ -317,6 +302,8 @@ read_fam_file(const char *fam_filename)
         if (pheno_val[15] == '\0') {
             if ((strlen(pheno_val) == 1) && (isdigit(pheno_val[0]))) {
                 new_node->phenotype_value = (char)atoi(pheno_val);
+            } else if (strcmp(pheno_val, "-9") == 0) {
+                new_node->phenotype_value = 0;
             } else if (strcmp(pheno_val, "NA") == 0) {
                 new_node->phenotype_value = 0;
             } else {
@@ -333,7 +320,7 @@ read_fam_file(const char *fam_filename)
         if (!first_node) {
             first_node = new_node;
             node_ptr = new_node;
-            data_out->current_node = new_node;
+            data_out->next_iter = new_node;
             data_out->fam_data = new_node;
             data_out->line_num++;
         } else {
@@ -342,15 +329,41 @@ read_fam_file(const char *fam_filename)
             data_out->line_num++;
         }
     }
-
+    fclose(fin);
     return data_out;
 }
 
 
-static BED_DATA_ptr
+int
+clean_fam_data(FAM_DATA_ptr data_in)
+{
+    if (!data_in) {
+        return 0;
+    }
+    if (data_in->node_ptr_array) {
+        free(data_in->node_ptr_array);
+        data_in->node_ptr_array = NULL;
+    }
+    if (data_in->next_iter) {
+        data_in->next_iter = NULL;
+    }
+    FAM_NODE_ptr last_node = NULL, next_node = NULL;
+    last_node = data_in->fam_data;
+    data_in->fam_data = NULL;
+    while (last_node) {
+        next_node = last_node->next;
+        free(last_node);
+        last_node = next_node;
+    }
+    free(data_in);
+    return 0;
+}
+
+
+BED_DATA_ptr
 init_bed_data_struct(const char * bed_filename, uint32_t indiv_num, uint32_t variant_num)
 {
-    FILE * fin = fopen(bed_filename, "r");
+    FILE *fin = fopen(bed_filename, "r");
     if (!fin) {
         fprintf(stderr, "open bed file failed.\n");
         return NULL;
@@ -383,28 +396,36 @@ init_bed_data_struct(const char * bed_filename, uint32_t indiv_num, uint32_t var
     data_out->filehandle = fin;
     data_out->current_file_pos = 3;
     data_out->current_variant = 0;
-    data_out->data = (char *) malloc(ceil((double)indiv_num / 4) * 4);
+    data_out->data = (char *)malloc(ceil((double)indiv_num / 4) * 4);
     data_out->read_buf_len = ceil((double)indiv_num / 4);
-    data_out->read_buf = (uint8_t *) malloc(ceil((double)indiv_num / 4));
+    data_out->read_buf = (uint8_t *)malloc(ceil((double)indiv_num / 4));
     return data_out;
 }
 
 
-static int
-read_bed_by_variant(BED_DATA_ptr data_in)
+char *
+read_bed_by_variant(BED_DATA_ptr data_in, char *ext_data_array)
 {
     if (data_in->current_variant >= data_in->variant_num) {
-        return 1;
+        return NULL;
     }
 
+    data_in->data_len = 0;
     uint32_t read_len = data_in->read_buf_len;
     if (fread(data_in->read_buf, sizeof(uint8_t), data_in->read_buf_len, data_in->filehandle) != data_in->read_buf_len) {
         fprintf(stderr, "read bed file by variant failed.\n");
-        return 2;
+        return NULL;
     }
     data_in->current_file_pos += data_in->read_buf_len;
     data_in->current_variant += 1;
 
+    char *store_array = NULL;
+    if (ext_data_array) {
+        store_array = ext_data_array;
+    } else {
+        store_array = data_in->data;
+        data_in->data_len = data_in->indiv_num;
+    }
     int i = 0, j = 0;
     uint8_t char_tmp;
     char Low_1_2b = 0,  Low_2_2b = 0, Low_3_2b = 0, Low_4_2b = 0;
@@ -415,25 +436,24 @@ read_bed_by_variant(BED_DATA_ptr data_in)
         Low_2_2b = (char_tmp & CharM2) >> 2;
         Low_3_2b = (char_tmp & CharM3) >> 4;
         Low_4_2b = char_tmp >> 6;
-        (data_in->data)[j] = CONVERT_GENO(Low_1_2b);
-        (data_in->data)[++j] = CONVERT_GENO(Low_2_2b);
-        (data_in->data)[++j] = CONVERT_GENO(Low_3_2b);
-        (data_in->data)[++j] = CONVERT_GENO(Low_4_2b);
+        store_array[j] = CONVERT_GENO(Low_1_2b);
+        store_array[++j] = CONVERT_GENO(Low_2_2b);
+        store_array[++j] = CONVERT_GENO(Low_3_2b);
+        store_array[++j] = CONVERT_GENO(Low_4_2b);
 
         j++;
     }
     if (j != data_in->indiv_num) {
         fprintf(stderr, "decode genotype failed.\n");
-        return 3;
+        return NULL;
     }
-    data_in->data_len = data_in->indiv_num;
-    
-    return 0;
+
+    return store_array;
 }
 
 
-static int
-final_bed_data_struct(BED_DATA_ptr data_in)
+int
+finalize_bed_data_struct(BED_DATA_ptr data_in)
 {
     if (data_in) {
         if (data_in->filehandle) {
@@ -456,17 +476,19 @@ final_bed_data_struct(BED_DATA_ptr data_in)
 int
 main(int argc, char *argv[])
 {
-    char file_name[512];
-    int file_prefix_len = strlen(argv[1]);
-    strcpy(file_name, argv[1]);
-    file_name[file_prefix_len] = '\0';
-    strcat(file_name, ".bed");
+    const char *bed_name = argv[1];
+    uint32_t indi_num = 360;
+    uint32_t vari_num = 9596452;
+    BED_DATA_ptr bed_data = init_bed_data_struct(bed_name, 360, 9596452);
+    for (int i = 0; i < 100; i++) {
+        read_bed_by_variant(bed_data, NULL);
+        for (int j = 0; j < indi_num; j++) {
+            printf("%d ", (bed_data->data)[j]);
+        }
+        printf("\n");
+    }
 
-    BED_DATA_ptr data = init_bed_data_struct(file_name, 360, 9596452);
-    uint32_t indiv_num = data->indiv_num;
-    uint32_t variant_num = data->variant_num;
-    int status = read_bed_by_variant(data);
-
+    return 0;
 }
-
 #endif
+
