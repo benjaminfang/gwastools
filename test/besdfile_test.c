14df8b0c16060962a36cda4855b316be38edc706
Benjamin Fang <benjaminfang.ol@outlook.com>
2023-02-24 17:55:03 +0800

    add besdfile test

---
+++ test/besdfile_test.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#include "../lib/besdfile.h"
+
+
+int
+main(int argc, char *argv[])
+{
+    const char *fname = argv[1];
+    BESDFILE besd_data;
+
+    besdfileopen(fname, &besd_data);
+    uint32_t variant_num = besd_data.variant_num;
+    uint32_t probe_num = besd_data.probe_num;
+    uint32_t *index_buf = (uint32_t *)malloc(sizeof(uint32_t) * variant_num);
+    float *beta_buf = (float *)malloc(sizeof(float) * variant_num);
+    float *se_buf = (float *)malloc(sizeof(float) * variant_num);
+    uint32_t read_len;
+    besdreaddata(&besd_data, index_buf, beta_buf, se_buf, variant_num, &read_len);
+    for (uint32_t i = 0; i < read_len; i++) {
+        printf("%u %f %f\n", index_buf[i], beta_buf[i], se_buf[i]);
+    }
+
+
+
+
+
+    return 0;
+}
